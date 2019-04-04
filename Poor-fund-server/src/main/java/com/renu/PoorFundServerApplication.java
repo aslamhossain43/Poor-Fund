328bf335864e7fa2b78a8c9936c767dcd4233005
Atif Aslam <aslam.renu.soft43@gmail.com>
2019-04-04 10:17:46 +0600

    commit success

---
+++ Poor-fund-server/src/main/java/com/renu/PoorFundServerApplication.java
@@ -0,0 +1,13 @@
+package com.renu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoorFundServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoorFundServerApplication.class, args);
+	}
+
+}
